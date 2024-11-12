fb1180753d85fa79ab53c01062e6abcdc2214544
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2024-11-12 16:25:31 +0100

    Initial commit

---
+++ js/task-4.js
@@ -0,0 +1,38 @@
+function getShippingCost(country){
+    let price;
+
+    switch(country){
+        case "China":
+            country = "China"
+            price = 100;
+            return(`Shipping to ${country} will cost ${price} credits`);
+
+        case "Chile":
+            country = "Chile"
+            price = 250;
+            return(`Shipping to ${country} will cost ${price} credits`);
+            
+        case "Australia":
+            country = "Australia"
+            price = 170;
+            return(`Shipping to ${country} will cost ${price} credits`)
+
+        case "Jamaica":
+            country = "Jamaica"
+            price = 120;
+            return(`Shipping to ${country} will cost ${price} credits`)
+
+        default:
+            return('Sorry, there is no delivery to your country');
+    }
+}
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
+
+
+
